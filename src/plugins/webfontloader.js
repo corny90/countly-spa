28a40e72e7dfb46afd077efbe523dcd3e5ee6293
Simin Cornel Ciocan <ciocan.simin.cornel@gmail.com>
2024-04-07 23:36:39 +0300

    Add corporative font as general

--- src/plugins/webfontloader.js
+++ src/plugins/webfontloader.js
@@ -9,7 +9,7 @@ export async function loadFonts () {
 
   webFontLoader.load({
     google: {
-      families: ['Roboto:100,300,400,500,700,900&display=swap'],
+      families: ['Inter:wght@100..900&display=swap', 'Roboto:100,300,400,500,700,900&display=swap'],
     },
   })
 }
